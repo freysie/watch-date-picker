eb7bc77b5ad3763eefeab2c54931644797c6e33c
Freya Ariel <hifreyaariel@icloud.com>
2022-06-20 17:09:16 +0200

    Tweak minutiae

--- Sources/WatchDatePicker/TimePickerView.swift
+++ Sources/WatchDatePicker/TimePickerView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 // TODO: use locale-specific time separators (like “.” instead of “:”)
-// TODO: selection indicator with default size is cut off by status bar when in 12 o’clock position
+// TODO: selection indicator with default size has like 2 px cut off by status bar when in 12 o’clock position
+// TODO: figure out difference between `calendar` and `locale.calendar` (gregorian (current) and gregorian (fixed))
 
 /// A control for the inputting of time values.
 ///
@@ -35,6 +36,7 @@ public struct TimePickerView: View {
   @Environment(\.datePickerSelectionIndicatorFill) private var selectionIndicatorFill
 
   @Environment(\.locale) private var locale
+  @Environment(\.calendar) private var calendar
   @Environment(\.dismiss) private var dismiss
   private enum HourPeriod: Int { case am = 0, pm = 12; var offset: Int { rawValue } }
   private enum Component { case hour, minute }
@@ -83,29 +85,14 @@ public struct TimePickerView: View {
   ///   - emphasizedMarkSize: …
   ///   - emphasizedMarkFill: …
   ///   - onCompletion: A callback that will be invoked when the operation has succeeded.
-  public init(
-    selection: Binding<Date>,
-    mode: DatePicker.Mode = .time,
-//    selectionIndicatorRadius: CGFloat? = nil,
-//    selectionIndicatorColor: Color? = nil,
-//    markSize: CGSize? = nil,
-//    markFill: AnyShapeStyle? = nil,
-//    emphasizedMarkSize: CGSize? = nil,
-//    emphasizedMarkFill: AnyShapeStyle? = nil,
-    onCompletion: ((Date) -> Void)? = nil
-  ) {
+  public init(selection: Binding<Date>, mode: DatePicker.Mode = .time, onCompletion: ((Date) -> Void)? = nil) {
     _selection = selection
     self.mode = mode
-//    self.selectionIndicatorRadius = selectionIndicatorRadius
-//    self.selectionIndicatorColor = selectionIndicatorColor
-//    self.markSize = markSize
-//    self.markFill = markFill
-//    self.emphasizedMarkSize = emphasizedMarkSize
-//    self.emphasizedMarkFill = emphasizedMarkFill
     self.onCompletion = onCompletion
     _hour = State(initialValue: locale.calendar.component(.hour, from: self.selection))
     _minute = State(initialValue: locale.calendar.component(.minute, from: self.selection))
     
+    // print((calendar, locale.calendar, calendar == locale.calendar))
     // FIXME: rework this now that we’re using environment:
 //    if !(twentyFourHour == true) {
 //      _hourPeriod = State(initialValue: hour <= 12 ? .am : .pm)
@@ -141,12 +128,11 @@ public struct TimePickerView: View {
     GeometryReader { geometry in
       labels(with: geometry)
       
-      if focusedComponent == .hour {
+      switch focusedComponent {
+      case .hour:
         marks(for: .hour, with: geometry)
         selectionIndicator(for: hour, multiple: hourMultiple, with: geometry)
-      }
-      
-      if focusedComponent == .minute {
+      case .minute:
         marks(for: .minute, with: geometry)
         selectionIndicator(for: minute, multiple: 60, with: geometry)
       }
@@ -258,8 +244,6 @@ public struct TimePickerView: View {
           .focusable()
           .digitalCrownRotation(
             hourBinding,
-            //            from: 0.0,
-            //            through: 12.0,
             from: -Double.infinity,
             through: Double.infinity,
             by: nil,
@@ -271,18 +255,6 @@ public struct TimePickerView: View {
         //            WKInterfaceDevice.current().play(.notification)
         //          }
         
-        //        Text(String(normalizedHour == 0 ? 12 : normalizedHour))
-        //          .focusable()
-        //          .digitalCrownRotation(
-        //            $hour,
-        //            from: 0.0,
-        //            through: 12.0,
-        //            by: nil,
-        //            sensitivity: .low,
-        //            isContinuous: true,
-        //            isHapticFeedbackEnabled: true
-        //          )
-        
         Text(":")
           .padding(.bottom)
         
@@ -291,8 +263,6 @@ public struct TimePickerView: View {
           .focusable()
           .digitalCrownRotation(
             minuteBinding,
-            //            from: 0.0,
-            //            through: 60.0,
             from: -Double.infinity,
             through: Double.infinity,
             by: nil,
